224135fbe0a153aa0785167281f9164d36c90ec9
ricky <rickymartin782@gmail.com>
2018-07-21 23:31:15 +0700

    tambah pas uda queuee

---
+++ app/src/main/java/com/leoky/queueeu/Activity/OnProgress.java
@@ -0,0 +1,15 @@
+package com.leoky.queueeu.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.leoky.queueeu.R;
+
+public class OnProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_progress);
+    }
+}
--- app/src/main/java/com/leoky/queueeu/Activity/QueueDetailActivity.java
+++ app/src/main/java/com/leoky/queueeu/Activity/QueueDetailActivity.java
@@ -1,13 +1,17 @@
 package com.leoky.queueeu.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.leoky.queueeu.R;
 
 public class QueueDetailActivity extends AppCompatActivity {
 
     public static String KEY_NAME = "KEY_NAME";
+    Button queuee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,17 @@ public class QueueDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_queue_detail);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getIntent().getStringExtra(KEY_NAME));
+
+        queuee=findViewById(R.id.btn_queuee);
+
+        queuee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QueueDetailActivity.this,OnProgress.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
